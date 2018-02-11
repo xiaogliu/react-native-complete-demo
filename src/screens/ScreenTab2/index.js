9807305e8f84caf7998fbce2a1308b4baf7ad4d8
liuxiaoguang <vincexgliu@gmail.com>
2018-02-11 18:03:30 +0800

    添加tab切换图标

--- src/screens/ScreenTab2/index.js
+++ src/screens/ScreenTab2/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import { Image } from "react-native";
 import view from "./view";
 
 export default class ScreenHome extends Component {
   static navigationOptions = {
     title: "TAB2",
+    tabBarIcon: ({ focused }) => {
+      const icon = focused ? require("../../assets/images/tab_discover_active.png") : require("../../assets/images/tab_discover.png");
+      return <Image source={icon} style={{ height: 22, width: 22 }} />;
+    },
   };
 
   constructor(props) {
