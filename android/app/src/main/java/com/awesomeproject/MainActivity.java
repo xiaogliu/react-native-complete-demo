747f6083f0138e3bad22712fa639b4849f6795bb
liuxiaoguang <vincexgliu@gmail.com>
2018-02-26 15:12:24 +0800

    设置桌面图标/修改app名称/设置启动页完成

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -1,8 +1,16 @@
 package com.awesomeproject;
 
+import android.os.Bundle; // 启动页
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // 启动页
 
 public class MainActivity extends ReactActivity {
+    // 启动页相关
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
